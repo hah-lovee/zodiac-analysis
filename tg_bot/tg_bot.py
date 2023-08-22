6e67cac0b8d55b9dcf7eee0186e7070514f5d495
hah-love <legyx1@gmail.com>
2023-08-23 00:26:30 +0300

    add zz and male questions

--- tg_bot/tg_bot.py
+++ tg_bot/tg_bot.py
@@ -4,16 +4,16 @@ import csv
 import os
 from dotenv import load_dotenv
 
-# Загрузка переменных окружения из файла .env
 load_dotenv()
 
 token = os.environ['TOKEN']
-
 bot = telebot.TeleBot(token)
 
 user_states = {}
 
 QUESTIONS = [
+    "Введите ваш знак зодиака",
+    "Введите ваш пол",
     "Question 1: What is 2 + 2?",
     "Question 2: enter a number from 1 to 5",
     "Question 3: enter a number from 1 to 5",
@@ -27,17 +27,36 @@ QUESTIONS = [
 
 ]
 
+zodiac_signs = [
+    "овен",
+    "телец",
+    "близнецы",
+    "рак",
+    "лев",
+    "дева",
+    "весы",
+    "скорпион",
+    # "змееносец",
+    "стрелец",
+    "козерог",
+    "водолей",
+    "рыбы",
+]
+
 @bot.message_handler(commands=['start'])
 def start(message):
     # markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
     # btn1 = types.KeyboardButton("👋 Поздороваться")
     # markup.add(btn1)
-    bot.send_message(message.from_user.id, "Привет, предлагаю пройти тест. Для большей информации нажми на /help")
+    bot.send_message(message.from_user.id, "Привет, предлагаю пройти тест."
+                      "Для большей информации нажми на /help")
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.from_user.id, "/start — Показывает вступительные слова\n/help — Выводит этот текст\n/test — начинает тест")
+    bot.send_message(message.from_user.id, "/start — Показывает вступительные слова\n"
+                     "/help — Выводит этот текст\n"
+                     "/test — начинает тест")
 
 @bot.message_handler(commands=['test'])
 def test(message):
@@ -49,8 +68,8 @@ def ask_next_question(chat_id):
     question_number = user_states[chat_id]['current_question']
     if question_number < len(QUESTIONS):
         question = QUESTIONS[question_number]
+        # Добавить кнопки нормальные к вопросам
         bot.send_message(chat_id, question)
-        user_states[chat_id]['current_question'] += 1
         user_states[chat_id]['waiting_for_answer'] = True
     else:
         bot.send_message(chat_id, "Спасибо за ответы!")
@@ -63,28 +82,44 @@ def ask_next_question(chat_id):
         user_states[chat_id]['answers'] = []
 
 
-
 @bot.message_handler(func=lambda message: user_states.get(message.chat.id, {}).get('waiting_for_answer'))
 def reply_with_answer(message):
     chat_id = message.chat.id
     user_answer = message.text
-    # Сделать отдельную функцию проверки, тк будут ещё ответы в виде строк(пол и знак зодиака)
-    if user_answer.isdigit() and int(user_answer) in range(1,6):
-        user_states[chat_id]['answers'].append(user_answer)
-    else:
-        user_states[chat_id]["current_question"] -= 1
-        bot.send_message(chat_id, "Попробуй число от 1 до 5")
-    
-
+    validate_answers(chat_id, user_answer)
     ask_next_question(chat_id)
 
+def validate_answers(chat_id, answer):
+    # обработка зз 
+    # сделать словарём, чтоб сразу кодировать в цифры
+    if user_states[chat_id]["current_question"] == 0:
+        if answer.lower() in zodiac_signs:
+            user_states[chat_id]['answers'].append(answer)
+            user_states[chat_id]['current_question'] += 1
+        else:
+            bot.send_message(chat_id, "такого нету :(")
+    # обработка пола
+    elif user_states[chat_id]["current_question"] == 1:
+        if answer.lower() == "м":
+            user_states[chat_id]['answers'].append(0)
+            user_states[chat_id]['current_question'] += 1
+        elif answer.lower() == "ж":
+            user_states[chat_id]['answers'].append(1)
+            user_states[chat_id]['current_question'] += 1
+        else:
+            bot.send_message(chat_id, "м/ж")
+    else:
+        if answer.isdigit() and int(answer) in range(1,6):
+            user_states[chat_id]['answers'].append(answer)
+            user_states[chat_id]['current_question'] += 1
+        else:
+            # user_states[chat_id]["current_question"] -= 1
+            bot.send_message(chat_id, "Попробуй число от 1 до 5")
+        
 
 @bot.message_handler(func=lambda message: True)
 def handle_unrecognized_commands(message):
     bot.send_message(message.chat.id, "Извините, я не понимаю эту команду.")
 
-# @bot.message_handler(content_types=['text'])  #реагирует на любые сообщения
-# def text(message):
-#     pass
-
+print("Starting...")
 bot.polling(none_stop=True)
